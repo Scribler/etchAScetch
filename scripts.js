a1e16d1de12db4106c14aebe380c4dc6e5d44111
Steven McIntyre <steven.mc2225@gmail.com>
2023-09-23 22:58:26 -0700

    testing

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const body = document.querySelector('body');
+const etchascetchContainer = document.createElement('div');
+etchascetchContainer.classList.add('box');
+etchascetchContainer.textContent = "Test";
+
+body.appendChild(etchascetchContainer);
+
+
+
+
